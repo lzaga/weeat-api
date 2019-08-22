f8820a1bb6e210bedc2751331bd514ab826a2c2e
lzaga <linoy.zaga@wework.com>
2019-08-22 09:45:37 +0300

    Add Restaurant model & controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,44 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+
+    render json: @restaurants
+  end
+
+  def show
+    render json: @restaurant
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      render json: @restaurant, status: :created, location: @restaurant
+    else
+      render json: @restaurant.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      render json: @restaurant
+    else
+      render json: @restaurant.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+  end
+
+  private
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :cuisine, :rating, :ten_bis, :address, :max_delivery_time)
+    end
+end
