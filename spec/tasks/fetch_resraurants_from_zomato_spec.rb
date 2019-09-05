64eef7246fdd8381682ca7f3728f075c7b3066fc
lzaga <linoy.zaga@wework.com>
2019-09-05 14:20:02 +0300

    WIP - tests with vcr

--- spec/tasks/fetch_resraurants_from_zomato_spec.rb
+++ spec/tasks/fetch_resraurants_from_zomato_spec.rb
@@ -1,43 +1,43 @@
 require 'spec_helper'
 require 'rake'
 
-describe 'fetch_restaurants_from_zomato', :vcr  do
-  before(:all) do
-    Rake.application.rake_require("../../lib/tasks/fetch_restaurants_from_zomato")
-    Rake::Task.define_task(:environment)
-  end
+describe 'rake fetch_restaurants_from_zomato', type: task  do
+  # before(:all) do
+    # Rake.application.rake_require("../../lib/tasks/fetch_restaurants_from_zomato")
+    # Rake::Task.define_task(:environment)
+  # end
 
   before(:each) do
     Review.delete_all
     Restaurant.delete_all
   end
 
-  describe "test rake task" do
-    let :run_rake_task do
-      Rake::Task["fetch_restaurants_from_zomato"].reenable
-      Rake.application.invoke_task "fetch_restaurants_from_zomato"
-    end
-
-    it "should save new restaurants" do
-      VCR.use_cassette('fetch_restaurants') do
-        expect { run_rake_task }.to change { Restaurant.count }
-      end
-    end
-
-    it "should save new reviews" do
-      expect { run_rake_task }.to change { Review.count }
-    end
-
-    context 'errors' do
-      it "raises error if restaurant did not save" do
-        VCR.use_cassette('fetch_restaurants') do
-          allow(Restaurant).to receive(:find_or_create_by).and_raise(ActiveRecord::ActiveRecordError)
-
-          run_rake_task
-
-          expect { run_rake_task }.not_to change { Restaurant.count }
-        end
-      end
-    end
+  it 'preloads the Rails environment' do
+    expect(task.prerequisites).to include 'environment'
   end
+
+  describe "test rake task", :vcr do
+    it "should save new restaurants" do
+      expect { task.execute }.to change { Restaurant.count }
+    end
+
+    # it "should save new reviews" do
+    #   expect { run_rake_task }.to change { Review.count }
+    # end
+    #
+    # context 'errors' do
+    #   it "raises error if restaurant did not save" do
+    #     allow(Restaurant).to receive(:find_or_create_by).and_raise(ActiveRecord::ActiveRecordError)
+    #
+    #     run_rake_task
+    #
+    #     expect { run_rake_task }.not_to change { Restaurant.count }
+    #   end
+    # end
+  end
+end
+
+def run_rake_task
+  Rake::Task["fetch_restaurants_from_zomato"].reenable
+  Rake.application.invoke_task "fetch_restaurants_from_zomato"
 end
