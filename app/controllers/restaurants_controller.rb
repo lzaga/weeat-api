2d88b1cf275141662c65da18bea03141de721461
lzaga <linoy.zaga@wework.com>
2019-09-05 13:46:43 +0300

    Add filter

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,15 @@ class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :update, :destroy]
 
   def index
-    render json: Restaurant.all
+    if request.query_parameters.present?
+      params = create_where_params_obj(request.query_parameters)
+
+      @restaurants = Restaurant.where(params)
+    else
+      @restaurants = Restaurant.all
+    end
+
+    render json: @restaurants
   end
 
   def show
@@ -40,4 +48,22 @@ class RestaurantsController < ApplicationController
     def restaurant_params
       params.require(:restaurant).permit(:name, :cuisine, :rating, :ten_bis, :address, :max_delivery_time)
     end
+
+  def create_where_params_obj(query_params)
+    params = {}
+
+    if query_params['rating'].present?
+      params['rating'] = (query_params['rating'].to_i)..Restaurant::MAX_RATING
+    end
+
+    if query_params['cuisine'].present?
+      params['cuisine'] = query_params['cuisine']
+    end
+
+    if query_params['max_delivery_time'].present?
+      params['max_delivery_time'] = 0..(query_params['max_delivery_time'].to_i)
+    end
+
+    params
+  end
 end
